c1117310fa339f7f5a9984178f2e2f5bafe1480f
thinhjs <thinhit@yahoo.com>
2014-09-19 15:33:44 +0700

    init project

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+	app = express(),
+	server;
+
+
+app.set('PORT', 8080);
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('./public'));
+
+
+app.get('/', function (req, res){
+	res.render('index', {
+		title: 'Chat server using socket.io'
+	});
+
+});
+
+server = app.listen(app.get('PORT'), function (){
+	console.log('Server started at port '+ app.get('PORT'));
+});
+
+require('./socket_handler')(server);
+
---
+++ socket_handler.js
@@ -0,0 +1,3 @@
+module.exports = function (httpServer){
+	// TODO : 
+};
